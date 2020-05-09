a824b266a359174410508430fe90e3860cbe60f0
Rohit <srohit1101@gmail.com>
2020-05-09 22:21:42 +0530

    lists

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function ListItem(props) {
+  return <li>{props.value}</li>;
 }
 
-export default App;
+export function Numbers(props) {
+  return (
+    <ul>
+      {props.no.map((item) => (
+        <ListItem key={item.toString()} value={item} />
+      ))}
+    </ul>
+  );
+
+  // return <ul>{liItems}</ul>;
+}
